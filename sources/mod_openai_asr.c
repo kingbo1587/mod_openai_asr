f07b59bf1b3532a34052c0d2408f2540729ea1fd
shenjinbo <shenjinbo@weshare.com.cn>
2024-11-19 17:26:12 +0800

    remove curl.c and utils

--- sources/mod_openai_asr.c
+++ sources/mod_openai_asr.c
@@ -18,20 +18,209 @@
  *  Konstantin Alexandrin <akscfx@gmail.com>
  *
  *
- * Provides the ability to use OpenAI Speech-To-Text service in the Freeswitch.
- * https://platform.openai.com/docs/guides/speech-to-text
+ * mod_openai_asr.c -- openai speech-to-text service interface
  *
- * Development respository: 
- * https://github.com/akscf/mod_openai_asr
+ * Provides OpenAI STT service for the Freeswitch.
+ * https://platform.openai.com/docs/guides/speech-to-text
  *
  */
 #include "mod_openai_asr.h"
+
+
+
 
 globals_t globals;
 
 SWITCH_MODULE_LOAD_FUNCTION(mod_openai_asr_load);
 SWITCH_MODULE_SHUTDOWN_FUNCTION(mod_openai_asr_shutdown);
 SWITCH_MODULE_DEFINITION(mod_openai_asr, mod_openai_asr_load, mod_openai_asr_shutdown, NULL);
+
+
+
+switch_status_t xdata_buffer_alloc(xdata_buffer_t **out, switch_byte_t *data, uint32_t data_len) {
+    xdata_buffer_t *buf = NULL;
+
+    switch_zmalloc(buf, sizeof(xdata_buffer_t));
+
+    if(data_len) {
+        switch_malloc(buf->data, data_len);
+        switch_assert(buf->data);
+
+        buf->len = data_len;
+        memcpy(buf->data, data, data_len);
+    }
+
+    *out = buf;
+    return SWITCH_STATUS_SUCCESS;
+}
+
+void xdata_buffer_free(xdata_buffer_t **buf) {
+    if(buf && *buf) {
+        switch_safe_free((*buf)->data);
+        free(*buf);
+    }
+}
+
+void xdata_buffer_queue_clean(switch_queue_t *queue) {
+    xdata_buffer_t *data = NULL;
+
+    if(!queue || !switch_queue_size(queue)) {
+        return;
+    }
+
+    while(switch_queue_trypop(queue, (void *) &data) == SWITCH_STATUS_SUCCESS) {
+        if(data) { xdata_buffer_free(&data); }
+    }
+}
+
+switch_status_t xdata_buffer_push(switch_queue_t *queue, switch_byte_t *data, uint32_t data_len) {
+    xdata_buffer_t *buff = NULL;
+
+    if(xdata_buffer_alloc(&buff, data, data_len) == SWITCH_STATUS_SUCCESS) {
+        if(switch_queue_trypush(queue, buff) == SWITCH_STATUS_SUCCESS) {
+            return SWITCH_STATUS_SUCCESS;
+        }
+        xdata_buffer_free(&buff);
+    }
+    return SWITCH_STATUS_FALSE;
+}
+
+char *chunk_write(switch_byte_t *buf, uint32_t buf_len, uint32_t channels, uint32_t samplerate, const char *file_ext) {
+    switch_status_t status = SWITCH_STATUS_FALSE;
+    switch_size_t len = (buf_len / sizeof(int16_t));
+    switch_file_handle_t fh = { 0 };
+    char *file_name = NULL;
+    char name_uuid[SWITCH_UUID_FORMATTED_LENGTH + 1] = { 0 };
+    int flags = (SWITCH_FILE_FLAG_WRITE | SWITCH_FILE_DATA_SHORT);
+
+    switch_uuid_str((char *)name_uuid, sizeof(name_uuid));
+    file_name = switch_mprintf("%s%s%s.%s", globals.tmp_path, SWITCH_PATH_SEPARATOR, name_uuid, (file_ext == NULL ? "wav" : file_ext) );
+
+    if((status = switch_core_file_open(&fh, file_name, channels, samplerate, flags, NULL)) != SWITCH_STATUS_SUCCESS) {
+        switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR, "Unable to open file (%s)\n", file_name);
+        goto out;
+    }
+
+    if((status = switch_core_file_write(&fh, buf, &len)) != SWITCH_STATUS_SUCCESS) {
+        switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR, "Unable to write (%s)\n", file_name);
+        goto out;
+    }
+
+    switch_core_file_close(&fh);
+out:
+    if(status != SWITCH_STATUS_SUCCESS) {
+        if(file_name) {
+            unlink(file_name);
+            switch_safe_free(file_name);
+        }
+        return NULL;
+    }
+
+    return file_name;
+}
+
+static size_t curl_io_write_callback(char *buffer, size_t size, size_t nitems, void *user_data) {
+    switch_buffer_t *recv_buffer = (switch_buffer_t *)user_data;
+    size_t len = (size * nitems);
+
+    if(len > 0 && recv_buffer) {
+        switch_buffer_write(recv_buffer, buffer, len);
+    }
+
+    return len;
+}
+
+switch_status_t curl_perform(switch_buffer_t *recv_buffer, char *model_name, char *filename, globals_t *globals) {
+    switch_status_t status = SWITCH_STATUS_SUCCESS;
+    CURL *curl_handle = NULL;
+    curl_mime *form = NULL;
+    curl_mimepart *field1=NULL, *field2=NULL;
+    switch_curl_slist_t *headers = NULL;
+    switch_CURLcode curl_ret = 0;
+    long http_resp = 0;
+
+    curl_handle = switch_curl_easy_init();
+    headers = switch_curl_slist_append(headers, "Content-Type: multipart/form-data");
+
+    switch_curl_easy_setopt(curl_handle, CURLOPT_HTTPHEADER, headers);
+    switch_curl_easy_setopt(curl_handle, CURLOPT_POST, 1);
+    switch_curl_easy_setopt(curl_handle, CURLOPT_NOSIGNAL, 1);
+    switch_curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, curl_io_write_callback);
+    switch_curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void *) recv_buffer);
+
+    if(globals->connect_timeout > 0) {
+        switch_curl_easy_setopt(curl_handle, CURLOPT_CONNECTTIMEOUT, globals->connect_timeout);
+    }
+    if(globals->request_timeout > 0) {
+        switch_curl_easy_setopt(curl_handle, CURLOPT_TIMEOUT, globals->request_timeout);
+    }
+    if(globals->user_agent) {
+        switch_curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, globals->user_agent);
+    }
+    if(strncasecmp(globals->api_url, "https", 5) == 0) {
+        switch_curl_easy_setopt(curl_handle, CURLOPT_SSL_VERIFYPEER, 0);
+        switch_curl_easy_setopt(curl_handle, CURLOPT_SSL_VERIFYHOST, 0);
+    }
+    if(globals->proxy) {
+        if(globals->proxy_credentials != NULL) {
+            switch_curl_easy_setopt(curl_handle, CURLOPT_PROXYAUTH, CURLAUTH_ANY);
+            switch_curl_easy_setopt(curl_handle, CURLOPT_PROXYUSERPWD, globals->proxy_credentials);
+        }
+        if(strncasecmp(globals->proxy, "https", 5) == 0) {
+            switch_curl_easy_setopt(curl_handle, CURLOPT_PROXY_SSL_VERIFYPEER, 0);
+        }
+        switch_curl_easy_setopt(curl_handle, CURLOPT_PROXY, globals->proxy);
+    }
+
+    curl_easy_setopt(curl_handle, CURLOPT_XOAUTH2_BEARER, globals->api_key);
+    curl_easy_setopt(curl_handle, CURLOPT_HTTPAUTH, CURLAUTH_BEARER);
+
+    if((form = curl_mime_init(curl_handle))) {
+        if((field1 = curl_mime_addpart(form))) {
+            curl_mime_name(field1, "model");
+            curl_mime_data(field1, model_name, CURL_ZERO_TERMINATED);
+        }
+        if((field2 = curl_mime_addpart(form))) {
+            curl_mime_name(field2, "file");
+            curl_mime_filedata(field2, filename);
+        }
+        switch_curl_easy_setopt(curl_handle, CURLOPT_MIMEPOST, form);
+    }
+
+    headers = switch_curl_slist_append(headers, "Expect:");
+    switch_curl_easy_setopt(curl_handle, CURLOPT_URL, globals->api_url);
+
+    curl_ret = switch_curl_easy_perform(curl_handle);
+    if(!curl_ret) {
+        switch_curl_easy_getinfo(curl_handle, CURLINFO_RESPONSE_CODE, &http_resp);
+        if(!http_resp) { switch_curl_easy_getinfo(curl_handle, CURLINFO_HTTP_CONNECTCODE, &http_resp); }
+    } else {
+        http_resp = curl_ret;
+    }
+
+    if(http_resp != 200) {
+        switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR, "http-error=[%ld] (%s)\n", http_resp, globals->api_url);
+        status = SWITCH_STATUS_FALSE;
+    }
+
+    if(recv_buffer) {
+        if(switch_buffer_inuse(recv_buffer) > 0) {
+            switch_buffer_write(recv_buffer, "\0", 1);
+        }
+    }
+
+    if(curl_handle) {
+        switch_curl_easy_cleanup(curl_handle);
+    }
+    if(form) {
+        curl_mime_free(form);
+    }
+    if(headers) {
+        switch_curl_slist_free_all(headers);
+    }
+
+    return status;
+}
 
 static void *SWITCH_THREAD_FUNC transcribe_thread(switch_thread_t *thread, void *obj) {
     volatile asr_ctx_t *_ref = (asr_ctx_t *)obj;
@@ -343,9 +532,13 @@ static switch_status_t asr_feed(switch_asr_handle_t *ah, void *data, unsigned in
         }
 
         vad_state = switch_vad_process(asr_ctx->vad, (int16_t *)data, (data_len / sizeof(int16_t)));
+        // if(vad_state) {
+        //     switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_INFO, "ki log asr vad state %s\n", switch_vad_state2str(vad_state));
+        // }
         if(vad_state == SWITCH_VAD_STATE_START_TALKING) {
             asr_ctx->vad_state = vad_state;
             fl_has_audio = SWITCH_TRUE;
+            switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_INFO, "ki log asr vad start talking, session_uuid is %s\n", asr_ctx->session_uuid);
             if(asr_ctx->session_uuid){
                 // fire event for vad start talking
                 switch_event_t *event;
@@ -355,7 +548,7 @@ static switch_status_t asr_feed(switch_asr_handle_t *ah, void *data, unsigned in
                     DUMP_EVENT(event);
                     switch_event_fire(&event);
                 }
-            }
+            } 
         } else if (vad_state == SWITCH_VAD_STATE_STOP_TALKING) {
             asr_ctx->vad_state = vad_state;
             fl_has_audio = SWITCH_FALSE;
@@ -621,7 +814,7 @@ SWITCH_MODULE_SHUTDOWN_FUNCTION(mod_openai_asr_shutdown) {
     uint8_t fl_wloop = SWITCH_TRUE;
 
     globals.fl_shutdown = SWITCH_TRUE;
-
+        
     switch_mutex_lock(globals.mutex);
     fl_wloop = (globals.active_threads > 0);
     switch_mutex_unlock(globals.mutex);
